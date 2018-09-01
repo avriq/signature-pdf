ed8509ba5d02a979a4f281f7de2a45024f50620b
avriqq <ayr@rumahaplikasi.com>
2018-09-01 12:30:00 +0700

    added: add drag drop digital signature

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,19 @@
-$(document).ready(function () {
+$(document).ready(() => {
   'use strict';
 
   var documentToolbar = $('.document-toolbar')
   var contentHeader = $('.content-header')
   var distance = contentHeader.outerHeight()
-  console.log(distance)
   $('.document-toolbar-container-sticky').css('height', documentToolbar.outerHeight())
 
   $(window).scroll(() => {
-    console.log('test')
     if ($(window).scrollTop() >= distance) {
-      console.log('scroll: ' + $(window).scrollTop())
       documentToolbar.addClass('document-toolbar-sticky')
     } else {
       documentToolbar.removeClass('document-toolbar-sticky')
     }
-  });
-  if( $(window).scrollTop() > distance){
+  })
+  if ($(window).scrollTop() > distance) {
     documentToolbar.addClass('document-toolbar-sticky')
   }
-});
+})
---
+++ assets/js/signature.config.js
@@ -0,0 +1,26 @@
+interact('.digital-signature')
+  .draggable({
+    inertia: true,
+    restrict: {
+      restriction: "parent",
+      endOnly: true,
+      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+    },
+    autoScroll: true,
+    onmove: function (event) {
+      var target = event.target,
+          x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+          y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+      target.style.webkitTransform = target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+      target.style.border = '2px dashed #ddd';
+
+      target.setAttribute('data-x', x);
+      target.setAttribute('data-y', y);
+      console.log('Coordinate X,Y(' + event.pageX + ', ' + event.pageY + ')')
+    },
+    onend: function (event) {
+      var target = event.target;
+      target.style.border = '2px dashed transparent';
+    }
+  });
